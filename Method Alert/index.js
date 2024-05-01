7bbd09557461c2b4534e21400a56f820340a09dc
Aditya Wardana <wardanaaditya49@gmail.com>
2024-05-01 12:35:06 +0700

    First Commit

---
+++ Method Alert/index.js
@@ -0,0 +1,5 @@
+let message = 'Halo, user!';
+alert(message); //alert yang tidak ada Input dari USER hanya tanda ok
+
+let pesanInput = prompt('Silakan masukkan angka...');
+console.log(typeof pesanInput); // output -> 'string'
