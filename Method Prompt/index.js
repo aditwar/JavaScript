4858be57ca7f10e50a76e789cca85b3be7a9a150
Aditya Wardana <wardanaaditya49@gmail.com>
2024-05-01 13:13:59 +0700

    a

--- Method Prompt/index.js
+++ Method Prompt/index.js
@@ -1,5 +1,2 @@
-let message = 'Halo, user!';
-alert(message); //alert yang tidak ada Input dari USER hanya tanda ok
-
-let pesanInput = prompt('Silakan masukkan angka...');
-console.log(typeof pesanInput); // output -> 'string'
+let pesanInput = prompt('Tuhan Engkau sungguh baik');
+console.log(pesanInput);
