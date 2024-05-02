2b9dc9697c71a2cdb0454436c06537942e38af54
Aditya Wardana <wardanaaditya49@gmail.com>
2024-05-02 22:07:00 +0700

    a

--- Mengubah Konten/index.js
+++ Mengubah Konten/index.js
@@ -1,2 +1,14 @@
 // Memanggil elemen ById
 const links = document.getElementById('links');
+links.innerHTML // Jika diketik ini maka menangkap semua konten dalam sebuah elemen beserta tag-tag HTML yang ada
+links.innerText // Jika diketik ini maka hanya mengambil teks tanpa tag-tag HTML yang ada (Lebih singkat)
+
+
+// INI PRAKTEKNYA MENGUBAH ELEMEN HTML sesuai keinginan (innerText)
+const dicoding = document.getElementById('dicodingLink');
+dicoding // Ketik ini maka muncul
+undefined
+dicoding.innerText="Belajar JavaScript";
+// Maka link dari dicodingLink tadi berubah menjadi Belajar JavaScript
+
+// SELANJUTNYA PRAKTEK innerHTML di foldernya yaa
---
+++ innerHTML innerText/index.js
@@ -0,0 +1,6 @@
+// Menangkap elemen link dicoding dulu
+const dicoding = document.getElementById('dicodingLink');
+dicoding // Ketik ini maka muncul
+undefined // Muncul otomatis
+dicoding.innerHTML='<i>Belajar Programming di Dicoding</i>'; // Ketik ini 
+// Maka link dari dicodingLink tadi berubah menjadi Belajar JavaScript
