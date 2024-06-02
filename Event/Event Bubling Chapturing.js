93f0caae0bf276355ab5d1d49713e6a5813ec6f4
Aditya Wardana <wardanaaditya49@gmail.com>
2024-06-02 23:58:40 +0700

    a

--- Event/Event Bubling Chapturing.js
+++ Event/Event Bubling Chapturing.js
@@ -1,7 +1,7 @@
 const divs = document.getElementsByTagName('div');
 
-for (let el of divs){
+for (let el of divs){ // Penerapan berdasarkan child div
     el.addEventListener('click', function(){
-        alert('ELEMENT', + el.getAttribute('id').toUpperCase());
-    });
+        alert('ELEMENT ', + el.getAttribute('id').toUpperCase());
+    },true); // Event Listener yang memberikan parameter BOOLEAN KHUSUS sehingga akan memberikan urutan sebaliknya
 }
