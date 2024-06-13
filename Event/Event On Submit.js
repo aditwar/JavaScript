f0b0c4194b152f46c10643012a92f355dff4bb9f
Aditya Wardana <wardanaaditya49@gmail.com>
2024-06-13 09:34:34 -0700

    a

--- Event/Event On Submit.js
+++ Event/Event On Submit.js
@@ -1,10 +1,10 @@
 const submitAction = document.getElementById('formDataDiri');
 
-submitAction.addEventListener('submit',function(event){
+submitAction.addEventListener('submit', function(event){
     const inputNama = document.getElementById('inputNama').value;
     const inputDomisili = document.getElementById('inputDomisili').value;
     const hiddenMessage = 'Halo, ${inputNama}. Bagaimana cuacanya di ${inputDomisili}?'
 
     document.getElementById('messageAfterSubmit').innerText = hiddenMessage;
     event.preventDefault(); // ini untuk mencegah proses refresh browser
-})
+});
